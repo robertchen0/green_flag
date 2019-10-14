146fb9ff37844f44d1d5ddc0621e9d4dcb002877
laosrob <robertchen0@yahoo.com>
2019-10-14 18:03:17 -0400

    validations

--- app/src/main/java/com/example/greenflag/CreateAccount.java
+++ app/src/main/java/com/example/greenflag/CreateAccount.java
@@ -13,7 +13,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -24,148 +23,156 @@ public class CreateAccount extends AppCompatActivity {
     Button create_account;
     TextView error_email;
     TextView error_pw;
+    TextView error_cpw;
 
     private static final String TAG = "CreateAccount";
+    public boolean email = false;
+    public boolean pw = false;
+    public boolean cpw = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        if(getSupportActionBar()!=null){
+        if (getSupportActionBar() != null) {
             setTitle("Create Account");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-        
-        create_account = findViewById(R.id.reg_account_btn);
-        error_email = findViewById(R.id.error_email);
-        error_pw = findViewById(R.id.error_pw);
 
-        create_account.setOnClickListener(new View.OnClickListener() {
+        create_account = findViewById(R.id.reg_account_btn); //button to create account
+        error_email = findViewById(R.id.error_email); // error editText for email
+        error_pw = findViewById(R.id.error_pw); // error editText for password
+        error_cpw = findViewById(R.id.repeat_pw); //error editText for confirm password
+
+        reg_email = findViewById(R.id.email);
+        reg_password = findViewById(R.id.password);
+        confirm_password = findViewById(R.id.repeat_password);
+
+        reg_email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-
-                error_email.setVisibility(View.GONE);
-                error_pw.setVisibility(View.GONE);
-
-                reg_email = findViewById(R.id.email);
-                reg_password = findViewById(R.id.password);
-                confirm_password = findViewById(R.id.repeat_password);
-
-                String emailS = reg_email.getText().toString();
-                String pwS = reg_password.getText().toString();
-                String cpwS = confirm_password.getText().toString();
-                String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                        "[a-zA-Z0-9_+&*-]+)*@" +
-                        "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                        "A-Z]{2,7}$";
-
-
-                if(emailS.isEmpty()){
-                    Toast.makeText(CreateAccount.this,
-                            "Email cannot be Empty!",
-                            Toast.LENGTH_LONG).show();
-                    error_email.setText("Email cannot be Blank!");
-                    error_email.setVisibility(View.VISIBLE);
+            public void onFocusChange(View view, boolean hasFocus) {
+                email = false;
+                if (hasFocus) {
+                    error_email.setVisibility(View.GONE);
+                } else {
+                    String emailS = reg_email.getText().toString();
+                    if (emailS.isEmpty()) {
+                        error_email.setText("Email cannot be Blank!");
+                        error_email.setVisibility(View.VISIBLE);
+                        return;
+                    }
+                    if (isValid(emailS)) {
+                        email = true;
+                    } else {
+                        error_email.setText("Email not valid");
+                        error_email.setVisibility(View.VISIBLE);
+                    }
                 }
+            }
+        });
 
-                if(pwS.isEmpty()){
-                    Toast.makeText(CreateAccount.this,
-                            "Password cannot be Empty!",
-                            Toast.LENGTH_LONG).show();
-                    error_pw.setText("Password cannot be blank!");
-                    error_pw.setVisibility(View.VISIBLE);
-                    return;
-                }
-
-                if (emailS.trim().matches(emailPattern)) {
-                    Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
-
-                    if(pwS.length() < 8 ){
-                        Toast.makeText(getApplicationContext(),"Password must be greater than 8 chars", Toast.LENGTH_SHORT).show();
+        reg_password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                pw = false;
+                if (hasFocus) {
+                    error_pw.setVisibility(View.GONE);
+                } else {
+                    String pwS = reg_password.getText().toString();
+                    if (pwS.isEmpty()) {
+                        error_pw.setText("Password cannot be blank!");
+                        error_pw.setVisibility(View.VISIBLE);
+                        return;
+                    }
+                    if (pwS.length() < 8) {
                         error_pw.setText("Password must be greater than 8 chars");
                         error_pw.setVisibility(View.VISIBLE);
                         return;
                     }
-
-                    if(pwS.equals(cpwS)) {
-                        Log.d(TAG, "onCreate://////////////// email " + emailS + " Password " + pwS + " Confirm PW " + cpwS);
-                        create_account.setOnClickListener(w->{
-                            Intent openNewActivity = new Intent();
-                            openNewActivity.setClass(
-                                    CreateAccount.this,
-                                    AccountInformation.class);
-                            startActivityForResult(openNewActivity,
-                                    1);
-                        });
+                    if (isValidPassword(pwS)) {
+                        pw = true;
+                    } else {
+                        error_pw.setText("Password invalid");
+                        error_pw.setVisibility(View.VISIBLE);
                     }
-                    else{
-                        Toast.makeText(CreateAccount.this,
-                                    "Passwords do not match!",
-                                    Toast.LENGTH_LONG).show();
-                            error_pw.setText("Passwords do not match!");
-                            error_pw.setVisibility(View.VISIBLE);
-                            return;
+                }
+            }
+        });
+
+        confirm_password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                cpw = false;
+                if (hasFocus) {
+                    error_cpw.setVisibility(View.GONE);
+                } else {
+                    String pwS = reg_password.getText().toString();
+                    String cpwS = confirm_password.getText().toString();
+                    if (!pwS.equals(cpwS)) {
+                        error_cpw.setText("Password does not Match!");
+                        error_cpw.setVisibility(View.VISIBLE);
+                        return;
+                    } else {
+                        cpw = true;
                     }
-
-
-//                    if (isValidPassword(reg_password.getText().toString())) {
-//                        Toast.makeText(CreateAccount.this, "Valid", Toast.LENGTH_SHORT).show();
-//                        if(pwS.equals((cpwS))){
-//                            Log.d(TAG, "onCreate://////////////// email " + emailS + " Password " + pwS + " Confirm PW " + cpwS);
-//                            create_account.setOnClickListener(w->{
-//                                Intent openNewActivity = new Intent();
-//                                openNewActivity.setClass(
-//                                        CreateAccount.this,
-//                                        AccountInformation.class);
-//                                startActivityForResult(openNewActivity,
-//                                        1);
-//                            });
-//                        }
-//
-//                        if (!cpwS.equals(pwS))
-//                        {
-//                            Toast.makeText(CreateAccount.this,
-//                                    "Passwords do not match!",
-//                                    Toast.LENGTH_LONG).show();
-//                            error_pw.setText("Passwords do not match!");
-//                            error_pw.setVisibility(View.VISIBLE);
-//                            return;
-//                        }
-//                    }
-
-//                    if(!isValidPassword(reg_password.getText().toString())){
-//                        error_pw.setText("Passwords does not meet requirements!");
-//                        error_pw.setVisibility(View.VISIBLE);
-//                    }
-
                 }
-                else {
-                    Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
-                    error_email.setText("Invalid email address");
-                    error_email.setVisibility(View.VISIBLE);
-                }
-            }});
+            }
+        });
 
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailS = reg_email.getText().toString();
+                String pwS = reg_password.getText().toString();
+                String cpwS = confirm_password.getText().toString();
+
+                if (isValid(emailS) && (isValidPassword(pwS)) && pwS.equals(cpwS)){
+                    email = true;
+                    pw = true;
+                    cpw = true;
+                    create_account.setOnClickListener(w -> {
+                        Intent newActivity = new Intent(getApplicationContext(), AccountInformation.class);
+                        startActivity(newActivity);
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public boolean isValidPassword(final String password) {
+    public static boolean isValid(String email) // Email validation
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
 
-        Pattern pattern;
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
 
-//        final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z]))";
-        final String PASSWORD_PATTERN = "(?=.*[0-9])$";
-        pattern = Pattern.compile(PASSWORD_PATTERN);
+    public static boolean isValidPassword(String password) { // Password validation
 
-        return pattern.matcher(password).matches();
+        String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{8,}$";
+
+        Pattern pat = Pattern.compile(PASSWORD_PATTERN);
+        if (password == null)
+            return false;
+
+        return pat.matcher(password).matches();
 
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId() == android.R.id.home)
+        if (item.getItemId() == android.R.id.home)
             finish();
 
         return super.onOptionsItemSelected(item);
